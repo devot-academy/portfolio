3e9848037f019785ef3b32cc371a9329049e72b1
Raissa Marques <raissatkm17@gmail.com>
2023-06-14 18:14:00 -0300

    global

---
+++ global/theme/fonts/interfaces.ts
@@ -0,0 +1,10 @@
+import { type FlattenSimpleInterpolation } from 'styled-components';
+
+export interface FontFormat {
+  THIN: FlattenSimpleInterpolation;
+  LIGHT: FlattenSimpleInterpolation;
+  REGULAR: FlattenSimpleInterpolation;
+  MEDIUM: FlattenSimpleInterpolation;
+  SEMI_BOLD: FlattenSimpleInterpolation;
+  BOLD: FlattenSimpleInterpolation;
+}
