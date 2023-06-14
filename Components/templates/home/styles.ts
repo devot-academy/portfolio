360dc047fec2d63c59d914f5051b92659f7bb4d4
Raissa Marques <raissatkm17@gmail.com>
2023-06-14 15:30:02 -0300

    templates

---
+++ Components/templates/home/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.div``;
+
+export const Navbar = styled.nav``;
+
+export const Main = styled.main``;
+
+export const TextComponent = styled.p``;
+
+export const ImageComponent = styled.img``;
+
+export const ButtonComponent = styled.button``;
+
+export const SocialMedia = styled.div``;
