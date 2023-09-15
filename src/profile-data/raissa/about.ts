f7f397d9f8d46e493cd709aeb03161a3934a493a
Álefe Cruz <alefecrz@gmail.com>
2023-09-15 19:14:11 -0300

    feat: create profile data from raissa

---
+++ src/profile-data/raissa/about.ts
@@ -0,0 +1,13 @@
+import { Props } from '@/components/templates/about'
+
+export const ABOUT_PROPS: Props = {
+    mainTitle: "About Me",
+    mainText: "Olá, sou Raissa Marques, 24 anos, atualmente focada na transição para a área de programação. Estou aprendendo TypeScript, Design System e Design Atômico. Faço parte da equipe Devot, onde trabalhamos com sprints semanais em projetos colaborativos. Minhas habilidades de comunicação e trabalho em equipe estão direcionadas para criar soluções inovadoras enquanto trilho meu caminho de sucesso na tecnologia.",
+    secondaryTitle: "My road",
+    secondaryText: "Minha trajetória profissional teve início com uma graduação em Pedagogia. Durante esse período, desenvolvi habilidades valiosas em comunicação, trabalho em equipe e cultivando um profundo apreço pelo universo da educação. Contudo, à medida que o tempo passava, percebi uma nova paixão emergindo dentro de mim: a programação. O mundo tecnológico me cativou de maneira única, e assim, decidi abraçar essa mudança de carreira com total determinação.",
+    MyPhotoAboutProps: {
+        src: 'https://avatars.githubusercontent.com/u/98561479?v=4',
+        alt: 'Myphoto',
+        hasBorderColor: 'SECONDARY',
+    },
+}
