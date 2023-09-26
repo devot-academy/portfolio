6240b668c6f266b0fc860e6ff7d89957434e6845
Raissa Marques <raissatkm17@gmail.com>
2023-09-26 14:20:43 -0300

    profile

--- src/profile-data/raissa/home.ts
+++ src/profile-data/raissa/home.ts
@@ -2,7 +2,7 @@ import { Props } from "@/components/templates/home";
 
 export const HOME_PROPS: Props = {
     title: 'react developer',
-    presentation: ' Tenho 24 anos e sou formada em pedagogia. Realizando,  transição para  a area de programação. Ganhei  experiência em trabalho em equipe nos ultimos 6 meses e estou determinada a me destacar nesse novo campo.'
+    presentation: ' Tenho 24 anos e sou formada em pedagogia. Realizando,  transição para  a area de programação. Ganhei  experiência em trabalho em equipe nos ultimos 6 meses e estou determinada a me destacar nesse novo campo.',
     ButtonPrimary: {
         label: 'CV',
         onClick: () => {},
